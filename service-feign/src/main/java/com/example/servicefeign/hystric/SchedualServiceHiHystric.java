01b5b57f6c30ba328ea6de2fff7ed936658ea0a2
lisc <121294134@qq.com>
2018-05-29 14:55:07 +0800

    add

---
+++ service-feign/src/main/java/com/example/servicefeign/hystric/SchedualServiceHiHystric.java
@@ -0,0 +1,12 @@
+package com.example.servicefeign.hystric;
+
+import com.example.servicefeign.service.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
